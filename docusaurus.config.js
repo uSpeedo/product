b4e6872966a175f49d54ac61af5ecb899ee0f3e7
pengfei.lv <pengfei.lv@ucloud.cn>
2023-02-02 15:52:21 +0800

    fix: config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: '',
+  url: 'https://umcloud-fe.github.io/product-docs/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/product-docs/',
